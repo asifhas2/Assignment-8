e3c44a82177c74a52f0194c12916c892215b1fc8
asifhas2 <ak1247942@gmail.com>
2026-03-29 02:25:01 +0600

    add inistall feture

---
+++ src/Components/InstallCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import downlodeIcon from '../assets/icon-downloads.png'
+import ratingIcon from '../assets/icon-ratings.png'
+import picture from '../assets/demo-app (3).webp'
+const InstallCard = ({data}) => {
+    const {title,size,ratingAvg,image,downloads}=data;
+    return (
+        <div className=' bg-white p-5 mt-6 flex justify-between items-center' >
+            <div className='flex  items-center gap-6'>
+                <div>
+                    <img className='h-30 rounded-2xl' src={image} alt="" />
+                </div>
+                <div className='space-y-5'>
+                    <h1 className='text-[20px] font-semibold'>{title}</h1>
+                    <div className='flex gap-8'>
+                        <p className='text-gray-400 flex gap-1 text-[18px] font-semibold'><img className='h-6' src={downlodeIcon} alt="" /> {downloads}M</p>
+                        <p className='text-gray-400 flex gap-1 text-[18px] font-semibold'><img className='h-6' src={ratingIcon} alt="" /> {ratingAvg}M</p>
+                        <p className='text-gray-400 flex gap-1 text-[18px] font-semibold'> {size}MB</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <button className='px-8 py-3 rounded-[6px] bg-green-400 text-white font-bold text-[18px] '>
+                    Uninstall
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default InstallCard;
--- src/Pages/Installation.jsx
+++ src/Pages/Installation.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import InstallCard from '../Components/InstallCard';
 
 const Installation = () => {
 
@@ -15,7 +16,22 @@ const Installation = () => {
 console.log(installApp);
     return (
         <div>
-            this is Installation page:{installApp.length}
+            <div className=' mt-20 space-y-7'>
+                    <h1 className='text-5xl font-bold text-center'>Your Installed Apps</h1>
+                    <p className='text-gray-500 text-center'>Explore All Trending Apps on the Market developed by us</p>
+
+            </div>
+            <div>
+                <div className='flex justify-between mt-10'>
+                    <h1 className='text-3xl font-bold'>{installApp.length} Apps Founds</h1>
+                    <button className='btn btn-primary'>sort by</button>
+                </div>
+                <div>
+                    {
+                        installApp.map(data =><InstallCard data={data}></InstallCard>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
